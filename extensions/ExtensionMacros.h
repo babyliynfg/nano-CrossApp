6fcf8020e77a60e9452e69f7256fe966694f4dfb
liyuanfeng <278688386@qq.com>
2016-10-09 15:09:32 +0800

    no message

--- extensions/ExtensionMacros.h
+++ extensions/ExtensionMacros.h
@@ -7,7 +7,7 @@
 #define NS_CC_EXT_END                       }
 #define USING_NS_CC_EXT                     using namespace extension
 
-#if 1
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32)
 #define CC_EXT_DLL     __declspec(dllexport)
 #else
 #define CC_EXT_DLL
